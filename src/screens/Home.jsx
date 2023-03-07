88f5bdd6251eff4468f26b23185ebff152cbbaac
akauf13 <adammkaufman10@gmail.com>
2023-03-07 13:22:21 -0500

    Edited Home

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -20,42 +20,48 @@ function Home() {
         <h1 className="title">Adam Kaufman's Portfolio</h1>
         <h2 className="intro">My Techstack:</h2>
         <div className="all">
-        <div className="techstack">
-              <Col xs={4} md={2} className="tech-icons">
-                <DiJavascript1 className="logo" />
-                <p style={{ fontSize: "1.2em" }}>JavaScript</p>
-              </Col>
-              <Col xs={4} md={2} className="tech-icons">
-                <DiNodejs className="logo" />
-                <p style={{ fontSize: "1.2em" }}>node.js</p>
-              </Col>
-              <Col xs={4} md={2} className="tech-icons">
-                <DiReact className="logo" />
-                <p style={{ fontSize: "1.2em" }}>React.js</p>
-              </Col>
-              <Col xs={4} md={2} className="tech-icons">
-                <DiPython className="logo" />
-                <p style={{ fontSize: "1.2em" }}>Python</p>
-              </Col>
-              <Col xs={4} md={2} className="tech-icons">
-                <DiDjango className="logo" />
-                <p style={{ fontSize: "1.2em" }}>Django</p>
-              </Col>
-              <Col xs={4} md={2} className="tech-icons">
-                <DiMongodb className="logo" />
-                <p style={{ fontSize: "1.2em" }}>MongoDB</p>
-              </Col>
-              <Col xs={4} md={2} className="tech-icons">
-                <DiPostgresql className="logo" />
-                <p style={{ fontSize: "1.2em" }}>PostgreSQL</p>
-              </Col>
-              <Col xs={4} md={2} className="tech-icons">
-                <DiGit className="logo" />
-                <p style={{ fontSize: "1.2em" }}>Git</p>
-              </Col>
+          <div className="techstack">
+            <Col xs={4} md={2} className="tech-icons">
+              <DiJavascript1 className="logo" />
+              <p style={{ fontSize: "1.2em" }}>JavaScript</p>
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+              <DiNodejs className="logo" />
+              <p style={{ fontSize: "1.2em" }}>node.js</p>
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+              <DiReact className="logo" />
+              <p style={{ fontSize: "1.2em" }}>React.js</p>
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+              <DiPython className="logo" />
+              <p style={{ fontSize: "1.2em" }}>Python</p>
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+              <DiDjango className="logo" />
+              <p style={{ fontSize: "1.2em" }}>Django</p>
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+              <DiMongodb className="logo" />
+              <p style={{ fontSize: "1.2em" }}>MongoDB</p>
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+              <DiPostgresql className="logo" />
+              <p style={{ fontSize: "1.2em" }}>PostgreSQL</p>
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+              <DiGit className="logo" />
+              <p style={{ fontSize: "1.2em" }}>Git</p>
+            </Col>
+          </div>
         </div>
+        <div className="look">
+          Checkout some of my{" "}
+          <NavLink to="/projects">
+            <span>Projects</span>
+          </NavLink>
+          .
         </div>
-        <div className="look">Checkout some of my <NavLink to="/projects"><span>Projects</span></NavLink>.</div>
       </div>
     </>
   );
