d373ddfe6d0ca221a6102fb534ee432cced3c97a
akauf13 <adammkaufman10@gmail.com>
2023-03-22 13:55:50 -0400

    new classNames

--- src/screens/Projects.jsx
+++ src/screens/Projects.jsx
@@ -21,6 +21,16 @@ function Projects() {
             </a>
           </div>
           <div className="proj2">
+            <h2>Word Guess</h2>
+            <img src={wordGuess} alt="Game Project" className="project" />
+            <a className="repo" href="https://github.com/akauf13/word-guess" target="_blank">
+          <h4>Github Repo</h4>
+            </a>
+            <a className="deploy" href="https://jovial-crumble-1db7ec.netlify.app/" target="_blank">
+          <h4>Deployed Link</h4>
+            </a>
+          </div>
+          <div className="proj3">
           <h2>$hills</h2>
             <img src={Shills} alt="Twitter Clone Project" className="project" />
             <a className="repo" href="https://github.com/SEI-Buffleheads/twitter-clone-frontend" target="_blank">
@@ -30,7 +40,7 @@ function Projects() {
           <h4>Deployed Link</h4>
             </a>
           </div>
-          <div className="proj3">
+          <div className="proj4">
             <h2>Gen 1 Pokemon</h2>
             <img src={Mern} alt="MERN Stack Project" className="project" />
             <a className="repo" href="https://github.com/akauf13/mern-project" target="_blank">
